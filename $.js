e2e95206137552f7fd353b4ad619a043901b33fc
Samuel Davidson <samuel@pixelninja.com>
2024-10-23 13:00:11 -0500

    Update $.js
    Added ability to import as module, and other minor enhancements.

--- $.js
+++ $.js
@@ -5,7 +5,7 @@
 // Released under the MIT License
 // https://github.com/pxninja/not-jquery/blob/main/LICENSE
 
-//-- SELECTOR FUNCTION --//
+//-- SELECTOR PROTOTYPE --//
 
 // This simplifies node selection in a jQuery-like fashion, dependency free.
 // Looks like jQuery, but is absolutely not jQuery and cannot be used with jQuery.
@@ -13,41 +13,39 @@
 // Ex: $('#myId')
 // Ex: $('#myId','.show')
 
-function $ (selector, attributes) {
-  if (!selector) return this;
-  if (typeof selector === 'string') {
-    let nodes = document.querySelectorAll(selector);
+Object.prototype.$ = function (selector = window, attributes) {
+
+  // Allow for array-like node selection, where $(0) is the <head>,
+  // $(1) is the <body>, $(1, 0) is the first <body> node, etc.
+  if ($_isNum(attributes)) attributes = String(attributes)
+  if ($_isNum(selector)) selector = $_child(document.querySelectorAll('html')[0], selector)
+
+  // Select the specified node(s)
+  if ($_isStr(selector)) {
+    let nodes = document.querySelectorAll(selector)
     switch (nodes.length) {
-      case 0 : selector = null;     break;
-      case 1 : selector = nodes[0]; break;
-      default: selector = nodes;    break;
+      case 0 : selector = null;     break
+      case 1 : selector = nodes[0]; break
+      default: selector = nodes;    break
     }
   }
-  return !attributes ? selector : selector.ƒ(attributes);
-};
+  return !attributes ? selector : selector.ƒ(attributes)
+}
 
+// -- ATTRIBUTE MANIPULATION PROTOTYPE -- //
 
-// -- ATTRIBUTE MANIPULATION FUNCTION -- //
-
-// This can be used on any object, dependency free, though it's
-// intended to support the $() selector function.
-
-// The 'ƒ' character was chosen to help avoid namespace collisions.
+// The 'ƒ' character was used to help avoid namespace collisions.
 // If you change the name of the prototype, also change it in
-// in the return line of the $() selector function.
+// the return line of the $() selector function.
 
 Object.prototype.ƒ = function (string) {
 
-  // IF the string is actually a number, treat it like an index
+  // If the string is actually a number, treat it like an index
   // and return the corresponding node, where 'obj.ƒ(0)'
   // is the first node, 'obj.ƒ(-1)'' is the last, etc.
 
-  const i = parseInt(string);
-  if (!isNaN(i) && typeof i === 'number') {
-    switch (i < 0) {
-      case true:  return this[this.length + i]; break;
-      case false: return this[i]; break;
-    }
+  if ($_isNum(string)) {
+    return $_child(this, string)
   }
 
 
@@ -58,30 +56,30 @@ Object.prototype.ƒ = function (string) {
   // number evaluation would never occur.
 
   // Exit if no object or string exist, or if the string is not a 'string'
-  if (!this || !string || typeof i === 'string') return this;
+  if (!this || !string || $_isStr(parseInt(string))) return this;
 
 
   // Because the first / last string characters are evaluated as operators,
   // leading / trailing white space needs to be removed.
 
   // Trim off leading / trailing white space
-  string = string.trim();
+  string = string.trim()
 
 
   // Set which characters will be evaluated as operation symbols.
-  const operator = {
+  let operator = {
     0: '±+-?',                    // Toggle, Add, Remove, Get
     1: string[0],                 // First character of string
     2: string[string.length - 1], // Last character of string
     3: string[string.length - 2]  // Second-to-last character
-  };
+  }
 
 
   // Putting the operator at the beginning of the string is a best practice.
   // Ex: obj.ƒ('+ .myClass')
 
   // Check if the first character is an operator.
-  if (operator[0].includes(operator[1])) return _doSomething.bind(this)(operator[1], 1, string.length);
+  if (operator[0].includes(operator[1])) return ƒ_doSomething.bind(this)(operator[1], 1, string.length)
 
 
   // Because '-' can be a valid string part, if the operator is
@@ -93,28 +91,28 @@ Object.prototype.ƒ = function (string) {
   // Ex: obj.ƒ('.myClass-')  => toggles 'myClass-'
 
   // Check if the last character is an operator preceded by a space.
-  if (operator[0].includes(operator[2]) && operator[3] === ' ') return _doSomething.bind(this)(operator[2], 0, -1);
+  if (operator[0].includes(operator[2]) && operator[3] === ' ') return ƒ_doSomething.bind(this)(operator[2], 0, -1)
 
 
   // If no operator was specified, toggle the inclusion / exclusion
   // of the specified attribute(s).
-  return _modify.bind(this)(_parse(string), '±');
+  return ƒ_modify.bind(this)(ƒ_parse(string), '±')
   
 
   // Helper function to execute a specific operation.
   // o - the operation to execute
   // a - starting index of slice
   // b - ending index of slice
-  function _doSomething (o,a,b) {
+  function ƒ_doSomething (o,a,b) {
 
     // Parse the string for attribute(s)
-    const s = _parse(string.slice(a,b));
+    let s = ƒ_parse(string.slice(a,b))
     
     // Check if GET is the intention
-    if (o === '?') return _get.bind(this)(s);
+    if (o === '?') return ƒ_get.bind(this)(s)
     
     // Otherwise MODIFY the attribute(s)
-    else return _modify.bind(this)(s, o);
+    return ƒ_modify.bind(this)(s, o)
   }
 
   // PARSE string for the space separated attribute(s)
@@ -126,21 +124,22 @@ Object.prototype.ƒ = function (string) {
   //   "data-blob": "some data",
   //   "disabled": true,
   //   "id": "myID" }
-  function _parse (string) {
-    let regex = /#([^\s#."']+)|\.([^\s#."']+)|([\w-]+)\s*=\s*["']([^"']+)["']|(\b[\w-]+\b)/g;
-    let match; const result = {};
+  function ƒ_parse (string) {
+    let result = {}
+    let regex = /#([^\s#."']+)|\.([^\s#."']+)|([\w-]+)\s*=\s*["']([^"']+)["']|(\b[\w-]+\b)/g
+    let match
     
     while ((match = regex.exec(string)) !== null) {
-      if (match[1]) result.id = match[1];
-      else if (match[2]) result.class = [...(result.class || []), match[2]];
+      if (match[1]) result.id = match[1]
+      else if (match[2]) result.class = [...(result.class || []), match[2]]
       else if (match[3] && match[4]) {
-        if (match[3] === 'class') result.class = match[4].split(/\s+/);
-        else result[match[3]] = match[4];
+        if (match[3] === 'class') result.class = match[4].split(/\s+/)
+        else result[match[3]] = match[4]
       }
-      else if (match[5]) result[match[5]] = '';
+      else if (match[5]) result[match[5]] = ''
     }
-    return result;
-  };
+    return result
+  }
 
   // MODIFY the element(s)
   // Any attribute value can be set with a 'key="val"' pattern.
@@ -149,28 +148,28 @@ Object.prototype.ƒ = function (string) {
   // Multiple attributes may be added/removed/toggled simultaneously.
   // Only one operation is executed.
   // NodeLists are supported.
-  function _modify (attributes, operation) {
-    elements = this instanceof NodeList ? this : [this];
-    elements.forEach(element => _art(element, attributes, operation));
-    return this;
+  function ƒ_modify (attributes, operation) {
+    let elements = this instanceof NodeList ? this : [this]
+    elements.forEach(element => ƒ_art(element, attributes, operation))
+    return this
   }
 
   // ADD / REMOVE / TOGGLE the attribute(s)
-  function _art (element, attributes, operation) {
-    for (const key in attributes) {
+  function ƒ_art (element, attributes, operation) {
+    for (let key in attributes) {
       if (attributes.hasOwnProperty(key)) {
-        const value = attributes[key];
+        let value = attributes[key]
         if (key === 'class') {
           switch (operation) {
-            case '+': value.forEach(className => element.classList.add(className));    break;
-            case '-': value.forEach(className => element.classList.remove(className)); break;
-            case '±': value.forEach(className => element.classList.toggle(className)); break;
+            case '+': value.forEach(className => element.classList.add(className));    break
+            case '-': value.forEach(className => element.classList.remove(className)); break
+            case '±': value.forEach(className => element.classList.toggle(className)); break
           }
         } else {
           switch (operation) {
-            case '+': element.setAttribute(key, value);    break;
-            case '-': element.removeAttribute(key, value); break;
-            case '±': element.getAttribute(key) === value ? element.removeAttribute(key) : element.setAttribute(key, value); break;
+            case '+': element.setAttribute(key, value);    break
+            case '-': element.removeAttribute(key, value); break
+            case '±': element.getAttribute(key) === value ? element.removeAttribute(key) : element.setAttribute(key, value); break
           }
         }
       }
@@ -178,119 +177,164 @@ Object.prototype.ƒ = function (string) {
   }
   
   // GET attribute(s)
-  // Returns either a string or bool, depending on the query ...
-  
-  // Returns an attributes value() as a string when a single attribute name
-  // is queried and the attribute exists.
-  // Ex: obj.ƒ('? class') => "classOne classTwo"
 
-  // If an attribute name exists, but no value exists, an empty string
-  // will return.
-  // Ex: obj.ƒ('? open') => ""
+  // Returns a single bool when a specific attribute value is queried, where true
+  // is when the queried value matches the atrribute value.
+  // Ex: obj.ƒ('? #myId') => bool
+  // Ex: obj.ƒ('? id="myId"') => bool
 
-  // Returns 'undefined' when a single attribute name is queried and the
+  // Specific to classes, returns a single bool representing the existence of,
+  // and not the order of, all queried classes, where true is when all the
+  // queried values exist, and false is when at least one does not,
+  // agnostic of query order.
+  // Ex: obj.ƒ('? .one') => bool - true for class="one two" and class="two one"
+  // Ex: obj.ƒ('? .one .two') => bool - true for class="one two" and class="two one"
+  // Ex: obj.ƒ('? .one .two') => bool - false for class="one" and class="two"
+
+  // Returns an attributes value when an attributes name is queried and
+  // the attribute value exists.
+  // Ex: obj.ƒ('? id') => 'myId'
+  // Ex: obj.ƒ('? class') => 'classOne classTwo'
+
+  // If an attribute exists with no value, an empty string is returned.
+  // Ex: obj.ƒ('? disabled') => ''
+
+  // Returns 'undefined' when an attribute name is queried and the
   // attribute name is absent.
   // Ex: obj.ƒ('? disabled') => undefined
 
-  // Returns 'false' when not all queried attributes or values are present.
-  // Ex: obj.ƒ('? id class') => false - either id or class is absent
-  // Ex: obj.ƒ('? .myClass') => false - myClass is absent
+  // Returns a key-value object when more than one attribute name is queried.
+  // Ex: obj.ƒ('? id class') => {id: 'myId', class: 'classOne classTwo'}
 
-  // Returns 'true'  when all queried attributes and values are present.
-  // Ex: obj.ƒ('? id class') => true - both id and class have values
-  // Ex: obj.ƒ('? .myClass') => true - myClass is present
+  // Returns a key-value object of all attributes when no specific
+  // attribute is queried.
+  // Ex: obj.ƒ('?') => {width: '640', height: '480'}
 
-  // If the operation is performed on a NodeList,
-  // results return in an array per node.
-  function _get (attributes) {
-    const result = [];
-    elements = this instanceof NodeList ? this : [this];
-    elements.forEach(element => result.push(_eval(element, attributes)));
-    return result.length > 1 ? result : result[0];
+  // And if the operation is performed on a NodeList, results are
+  // returned in an array.
+  // Ex: nodes.ƒ('? class') => ['classOne classTwo', 'classThree classFour']
+  // Ex: nodes.ƒ('?') [{...}, {...}]
+  
+  function ƒ_get (attributes) {
+    let result = []
+    let elements = this instanceof NodeList ? this : [this]
+    elements.forEach(element => result.push(ƒ_eval(element, attributes)))
+    return result.length > 1 ? result : result[0]
   }
 
   // EVALUATE attribute(s)
-  // A helper function containing the core _get() logic.
-  function _eval (element, attributes) {
-    let result = {};
-    for (const key in attributes) {
-      if (attributes.hasOwnProperty(key)) {
-        const value = attributes[key];
-        if (key === 'class' && Array.isArray(value)) {
-          value.forEach(className => result.class = [...(result.class || []), element.classList.contains(className)]);
-          result.class = result.class.every(boolean => boolean === true);
-        } else {
-          result[key] = element.getAttribute(key) ?? undefined;
-        }
-      }
-    }
+  // A helper function containing the core ƒ_get() logic.
+  function ƒ_eval (element, attributes) {
+    let result = {}
 
-    if (Object.keys(result).length > 1) {
+    // Immediately return if 'element' is not an Element
+    if (!(element instanceof Element)) return
 
-      for (const key in result) {
-        if (result.hasOwnProperty(key) && !result[key]) {
-          result = false;
-          break;
-        }
+    // If no specific query was made, get all attributes
+    if (Object.keys(attributes).length === 0) {
+      for (let i = 0; i < element.attributes.length; i++) {
+        result[element.attributes[i].name] = element.attributes[i].value
       }
 
-      if (result !== false) result = true;
-
-    } else if (Object.keys(result).length == 1) {
-
-      result = result[Object.keys(result)[0]];
-
+    // Otherwise get only the queried attributes
     } else {
-      result = false;
+      for (let key in attributes) {
+        if (attributes.hasOwnProperty(key)) {
+          let value = attributes[key]
+
+          // If querying for classes…
+          if (key === 'class' && Array.isArray(value)) {
+            value.forEach(className => result.class = [...(result.class || []), element.classList.contains(className)])
+            result.class = result.class.every(boolean => boolean === true)
+            continue
+          }
+
+          let elemValue = element.getAttribute(key)
+
+          // Get the attributes value when no query value is available to compare
+          if (!value) {
+            result[key] = elemValue ?? undefined
+            continue
+          }
+
+          // Otherwise compare the query and attribute values
+          result[key] = elemValue ? value === elemValue : undefined
+        }
+      }
     }
 
-    return result;
-  }
-};
+    // Return the most salient result
+    switch (Object.keys(result).length) {
+      case 0 : return undefined; break
+      case 1 : return result[Object.keys(result)[0]]; break
+      default: return result; break
 
+    }
+  }
+}
 
 
 //-- OTHER OBJECT PROTOTYPES --//
-// These can be used on any object, dependency free.
 
-// Helper functions for the object prototypes
-function ƒ_getNodeList (obj) {
-  return obj instanceof NodeList ? obj : [obj];
-}
-
-function ƒ_eventListener (obj, task, type, func) {
-  elems = ƒ_getNodeList(obj);
-  switch (task) {
-    case 0 : elems.forEach(elem => elem.addEventListener(type, func));    break;
-    case 1 : elems.forEach(elem => elem.removeEventListener(type, func)); break;
-    default: break;
-  }
-}
-
-
-// These are identical with the vanilla JS event listener
+// These are identical with the regular JS event listener
 // declarations, just more succinct with the capability
 // of looping through a node list.
 
 // Add listener
 Object.prototype.on = function (type, func) {
-  ƒ_eventListener(this, 0, type, func);
-  return this;
-};
+  $_eventListener(this, 0, type, func)
+  return this
+}
 
 // Remove listener
 Object.prototype.off = function (type, func) {
-  ƒ_eventListener(this, 1, type, func);
-  return this;
-};
+  $_eventListener(this, 1, type, func)
+  return this
+}
 
 // Set or get the innerHTML of a node
-Object.prototype.html = function (s,r) {
-  if (typeof s !== 'string') return !this.innerHTML ? false : this.innerHTML;
-  if (r)  return this.innerHTML = this.innerHTML.replace(s,r);
-  switch (s[0]){
-    case '+':  return this.innerHTML += s.trim().slice(1); break;
-    case '?':  return this.innerHTML; break;
-    default:   return this.innerHTML = s.trim(); break;
+Object.prototype.html = function (find, replace) {
+  return $_innerHTML(this, find, replace)
+}
+
+
+//-- HELPER FUNCTIONS --//
+
+function $_isNum(val) {
+  let i = parseInt(val)
+  return !isNaN(i) && typeof i === 'number'
+}
+
+function $_isStr(val) {
+  return typeof val === 'string'
+}
+
+function $_child(node, i) {
+  i = parseInt(i)
+  return i < 0 ? node.children[node.children.length + i] : node.children[i]
+}
+
+function $_getNodeList (obj) {
+  return obj instanceof NodeList ? obj : [obj]
+}
+
+function $_eventListener (obj = window, task, type, func) {
+  let elems = $_getNodeList(obj)
+  switch (task) {
+    case 0 : elems.forEach(elem => elem.addEventListener(type, func));    break
+    case 1 : elems.forEach(elem => elem.removeEventListener(type, func)); break
+    default: break
   }
 }
+
+function $_innerHTML (elem = document.documentElement, find, replace) {
+  if ($_isStr(find)) return !elem.innerHTML ? false : elem.innerHTML
+  if (replace) return elem.innerHTML = elem.innerHTML.replace(find, replace)
+  switch (find[0]){
+    case '+':  return elem.innerHTML += find.trim().slice(1); break
+    case '?':  return elem.innerHTML; break
+    default:   return elem.innerHTML = find.trim(); break
+  }
+}
+
+export default { $, ƒ, on, off, html }
